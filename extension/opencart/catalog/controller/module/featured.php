a486fd745b3e1c727fd3d0f56681496594158815
ypill <ypill@ns.kissenarose.com>
2022-05-07 17:09:31 +0000

    Fix Apache server "Option FollowSymlinks not allowed" and update extension module featured.php

--- extension/opencart/catalog/controller/module/featured.php
+++ extension/opencart/catalog/controller/module/featured.php
@@ -9,57 +9,60 @@ class Featured extends \Opencart\System\Engine\Controller {
 
 		$data['products'] = [];
 
-		$filter_data = [
-			'sort'  => 'pd.name',
-			'order' => 'ASC',
-			'start' => 0,
-			'limit' => $setting['limit']
-		];
+		if (isset($setting['product'])) {
+			$product_data = [];
 
-		$results = $this->model_catalog_product->getProducts($filter_data);
+			foreach ($setting['product'] as $product_id) {
+				$product_info = $this->model_catalog_product->getProduct($product_id);
 
-		if ($results) {
-			foreach ($results as $result) {
-				if ($result['image']) {
-					$image = $this->model_tool_image->resize(html_entity_decode($result['image'], ENT_QUOTES, 'UTF-8'), $setting['width'], $setting['height']);
+				if ($product_info) {
+					$products[] = $product_info;
+				}
+			}
+
+			foreach ($products as $product) {
+				if ($product['image']) {
+					$image = $this->model_tool_image->resize(html_entity_decode($product['image'], ENT_QUOTES, 'UTF-8'), $setting['width'], $setting['height']);
 				} else {
 					$image = $this->model_tool_image->resize('placeholder.png', $setting['width'], $setting['height']);
 				}
 
 				if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
-					$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+					$price = $this->currency->format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
 				} else {
 					$price = false;
 				}
 
-				if ((float)$result['special']) {
+				if ((float)$product['special']) {
 					$special = $this->currency->format($this->tax->calculate($product['special'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
 				} else {
 					$special = false;
 				}
 
 				if ($this->config->get('config_tax')) {
-					$tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
+					$tax = $this->currency->format((float)$product['special'] ? $product['special'] : $product['price'], $this->session->data['currency']);
 				} else {
 					$tax = false;
 				}
 
 				$product_data = [
-					'product_id'  => $result['product_id'],
+					'product_id'  => $product['product_id'],
 					'thumb'       => $image,
-					'name'        => $result['name'],
-					'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('config_product_description_length')) . '..',
+					'name'        => $product['name'],
+					'description' => utf8_substr(strip_tags(html_entity_decode($product['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('config_product_description_length')) . '..',
 					'price'       => $price,
 					'special'     => $special,
 					'tax'         => $tax,
-					'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
-					'rating'      => $result['rating'],
-					'href'        => $this->url->link('product/product', 'language=' . $this->config->get('config_language') . '&product_id=' . $result['product_id'])
+					'minimum'     => $product['minimum'] > 0 ? $product['minimum'] : 1,
+					'rating'      => (int)$product['rating'],
+					'href'        => $this->url->link('product/product', 'language=' . $this->config->get('config_language') . '&product_id=' . $product['product_id'])
 				];
 
 				$data['products'][] = $this->load->controller('product/thumb', $product_data);
 			}
+		}
 
+		if ($data['products']) {
 			return $this->load->view('extension/opencart/module/featured', $data);
 		} else {
 			return '';
