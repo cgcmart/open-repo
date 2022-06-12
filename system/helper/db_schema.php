64d1193b9a86640227f7719ad95d0501a7909ade
cgcmart <cgcmart@chinacenter.us>
2022-06-12 13:35:41 -0400

    update v4.0.0.0 2022-06-12

--- system/helper/db_schema.php
+++ system/helper/db_schema.php
@@ -4791,6 +4791,28 @@ function db_schema() {
 		'collate' => 'utf8mb4_general_ci'
 	];
 
+$tables[] = [
+		'name' => 'product_viewed',
+		'field' => [
+			[
+				'name' => 'product_id',
+				'type' => 'int(11)',
+				'not_null' => true
+			],
+			[
+				'name' => 'viewed',
+				'type' => 'int(11)',
+				'not_null' => true
+			]
+		],
+		'primary' => [
+			'product_id'
+		],
+		'engine' => 'InnoDB',
+		'charset' => 'utf8mb4',
+		'collate' => 'utf8mb4_general_ci'
+	];
+
 	$tables[] = [
 		'name' => 'return',
 		'field' => [
